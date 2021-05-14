489f28de41267f9b83bfd7d30daf91b4868e04db
vasanthvasu94 <84179269+vasanthvasu94@users.noreply.github.com>
2021-05-14 15:39:23 +0530

    initial commit

---
+++ CharacterCount.java
@@ -0,0 +1,54 @@
+package week1.day1.assignments;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CharacterCount {
+
+/*	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+  String str = "java";
+  Map<Character,Integer> CharMapCount = new HashMap<>();
+  for (Character c : str.toCharArray()){
+	  if (CharMapCount.containsKey(c)) {
+		  CharMapCount.put(c, CharMapCount.get(c) + 1);
+		  }
+	  else {CharMapCount.put(c, 1);
+	  
+	  }
+  }
+	System.out.println(CharMapCount);
+  }}*/
+	
+	public static void main(String[] args) {
+String str = "java";
+char[] c = str.toCharArray();
+Map<Character,Integer> map = new HashMap<>();
+for(int i=0;i<c.length;i++){
+	  if (map.containsKey(c[i])) {
+		  map.put(c[i], map.get((c)[i]) + 1);
+		  }
+	  else {map.put(c[i], 1);
+	  
+	  }
+}
+	System.out.println(map);
+}}
+	/*public static void main(String[] args) {
+		String input="java";
+		
+		char toFind = 'j';
+		
+	    int occ=0;
+		
+		for(int i=0;i<input.length();i++) {
+			
+		 if (input.charAt(i)==toFind)	{
+			 occ=occ+1;
+	
+		}
+		}
+	
+	System.out.println("character is found" +occ + "number of times");
+      	
+	}}*/
